f333c5d3c192f00be5d6fd9abb181957cd561d74
Tegar Machfudzi <v-tegarmachfudzi@anteraja.id>
2024-04-25 14:24:52 +0700

    first commit for landing page

---
+++ src/config/index.tsx
@@ -0,0 +1,26 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: "UI Kits",
+        value: "ui kits" as const,
+        featured: [
+            {
+                name: 'Editor picks',
+                href:"#",
+                imageSrc: "/nav/ui-kits/mixed.jpg"
+
+            }
+        ]
+    },
+    {
+        label: "Icons",
+        value: "Icons" as const,
+        featured: [
+            {
+                name: 'Editor picks',
+                href:"#",
+                imageSrc: "/nav/ui-kits/mixed.jpg"
+
+            }
+        ]
+    }
+]
